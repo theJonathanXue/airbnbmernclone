35199aef418385af50f62f56749e144ee3a197c2
Jonathan Xue <jonathanxue@ymail.com>
2023-01-03 13:58:23 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
