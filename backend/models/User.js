648e4fe56fa78fcb53776491c0a1002bf8df6fcb
Jonathan Xue <jonathanxue@ymail.com>
2023-01-03 15:31:52 -0500

    edit user

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required: true
+    },
     role: {
         type: String,
         enum: {
